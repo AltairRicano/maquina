69034b078bc43106b5ae5081f903a2882b09a05b
Altair Ricaño <zs24016703@estudiantes.uv.mx>
2025-10-23 15:06:13 -0600

    update1

--- src/Impresora.java
+++ src/Impresora.java
@@ -1,12 +1,36 @@
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 public class Impresora {
-    private String papel;
     private boolean hayTinta = true;
     private ArrayList< SimpleEntry<String, Integer> > tiposPapel = new  ArrayList<>();
 
-    public Impresora(String papel){
-        this.papel = papel;
+    public Impresora() {
     }
 
+    public void meterPapel(String tipo, int cantidad) {
+        for (SimpleEntry<String, Integer> entry : tiposPapel) {
+            if (entry.getKey().equals(tipo)) {
+                entry.setValue(entry.getValue() + cantidad);
+                return;
+            }
+        }
+        tiposPapel.add(new SimpleEntry<>(tipo, cantidad));
+    }
+
+    public void meterTinta() {
+        hayTinta = true;
+    }
+
+    public boolean tieneTinta() {
+        return hayTinta;
+    }
+
+    public boolean tieneBilletes(int cantidadNecesaria) {
+        for (SimpleEntry<String, Integer> entry : tiposPapel) {
+            if (entry.getValue() >= cantidadNecesaria) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
