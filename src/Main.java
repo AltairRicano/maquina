4a94596091e7c4dd77e707d03e43219be73462ca
Altair Ricaño <zs24016703@estudiantes.uv.mx>
2025-10-23 15:07:41 -0600

    Add main activity

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
+        Monedero monedero = new Monedero();
+        Impresora impresora = new Impresora();
+        System.out.println("Cantidad en el monedero: " + monedero.cantidadIntroducida());
+        impresora.meterPapel("A4", 100);
+        System.out.println("La impresora tiene tinta: " + impresora.tieneTinta());
+        System.out.println("La impresora tiene al menos 50 hojas: " + impresora.tieneBilletes(50));
 
     }
 }
